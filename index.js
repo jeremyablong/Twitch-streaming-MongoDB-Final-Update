98eaa9f34180de9aa32906a1bb39ce8854f98959
Jeremy Blong <jeremyblong@MacBook-Pro-3.local>
2019-06-12 19:54:27 -0400

    fixed route

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ mongoose.connect(keys.olderMongoURI, {
 	console.log("NOT connected to the database", error);
 });
 
-app.get("/", () => {
+app.get("/", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
